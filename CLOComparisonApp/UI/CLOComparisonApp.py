a0f581abd33fae5cb08a3adc4985503255a39cd7
Akshun <akshunchauhan099@gmail.com>
2024-12-15 15:26:30 -0700

    added about and help tab

--- CLOComparisonApp/UI/CLOComparisonApp.py
+++ CLOComparisonApp/UI/CLOComparisonApp.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import QMainWindow, QVBoxLayout, QWidget, QMessageBox
+from PyQt5.QtWidgets import QMainWindow, QVBoxLayout, QWidget, QMessageBox, QToolBar, QAction
+from PyQt5.QtCore import Qt
 from UI.tabs.SettingsTab import SettingsTab
 from .ResultTabsWidget import ResultTabsWidget
 from .ActionBarWidget import ActionBarWidget
@@ -38,11 +39,75 @@ class CLOComparisonApp(QMainWindow):
         container.setLayout(main_layout)
         self.setCentralWidget(container)
 
+        # Add a toolbar with Help and About actions
+        self.add_toolbar()
+
         # Connections
         self.action_bar_widget.button_compare.clicked.connect(self.compare_clos)
         self.settings_tab.file_selection_widget.update_files_signal.connect(
             self.action_bar_widget.update_file_paths
         )
+
+    def add_toolbar(self):
+        """Add a toolbar with Help and About buttons."""
+        toolbar = QToolBar("Toolbar", self)
+        toolbar.setMovable(False)  # Make the toolbar static
+
+        # Create Help action
+        help_action = QAction("Help", self)
+        help_action.triggered.connect(self.show_help_popup)
+        
+        # Create About action
+        about_action = QAction("About", self)
+        about_action.triggered.connect(self.show_about_popup)
+
+        # Add actions to the toolbar
+        toolbar.addAction(help_action)
+        toolbar.addAction(about_action)
+        self.addToolBar(Qt.TopToolBarArea, toolbar)
+
+    def show_help_popup(self):
+        """Show a popup box with instructions on how to use the application."""
+        instructions = (
+            "Welcome to the TRACE application!\n\n"
+            "Step 1: Select the existing files, then the new file.\n"
+            "Step 2: Set the thresholds using the sliders.\n"
+            "Step 3: Hit the 'Start Compare' button to begin the comparison process.\n\n"
+            "Note:\n"
+            "- The application may take some time to run based on your system specifications.\n"
+            "- In the existing file, ensure that one Excel file contains all the sheets that need to be compared.\n"
+            "- In the new file, ensure it contains only one sheet. All sheets must follow the standard structure.\n\n"
+            "Thresholds:\n"
+            "- The first threshold is the 'low-level threshold,' which specifies the percentage match required for each sentence.\n"
+            "- The second threshold is the 'high-level threshold,' which checks the average similarity of the entire sheet (usually containing 5 to 6 CLOs).\n\n"
+            "If the application crashes or the comparison process takes too long, please contact the developer at:\n"
+            "akshun.chauhan@rdpolytech.ca"
+        )
+        QMessageBox.information(self, "Help", instructions)
+
+
+    def show_about_popup(self):
+        """Show a popup box with information about the application."""
+        about_text = (
+            "<p>TRACE stands for <b>Total Recognition and Course Evaluation</b>. It is an internal tool specifically designed to identify duplications in courses using Course Learning Outcomes (CLOs). "
+            "This innovative application leverages machine learning to analyze and find similarities between sentences, utilizing the <i>all-MiniLM-L6-v2</i> sentence transformer model.</p>"
+            "\n\n"
+            "<p>As a prototype, TRACE is adaptable and can be further modified to meet specific needs. It was developed as a capstone project by <b>Akshun Chauhan</b> under the supervision of "
+            "<b>Dr. Rashid jalal Qureshi</b>.</p>"
+            "\n\n"
+            "<p>To learn more about the project or view the source code, visit the following links:</p>"
+            "<ul>"
+            "<li><a href='https://github.com/AkshunChauhan/TRACE.git'>GitHub: github.com/AkshunChauhan/TRACE.git</a></li>"
+            "<li><a href='https://www.linkedin.com/in/akshunchauhan/'>LinkedIn: linkedin.com/in/akshunchauhan/</a></li>"
+            "</ul>"
+        )
+
+        msg_box = QMessageBox(self)
+        msg_box.setWindowTitle("About TRACE")
+        msg_box.setTextFormat(Qt.RichText)  # Enables HTML formatting
+        msg_box.setText(about_text)
+        msg_box.exec_()
+
 
     def compare_clos(self):
         # Get file paths from the UI
