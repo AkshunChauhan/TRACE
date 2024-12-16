1e78dda316ef5a401ca65b887902d1e634c86da7
Akshun <akshunchauhan099@gmail.com>
2024-12-15 17:21:22 -0700

    added virtual env

--- CLOComparisonApp/main.py
+++ CLOComparisonApp/main.py
@@ -5,7 +5,7 @@ from UI.CLOComparisonApp import CLOComparisonApp
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    app.setWindowIcon(QIcon('TRACE\\CLOComparisonApp\\icons\\Logo.jpg'))
+    app.setWindowIcon(QIcon('icons/Logo.jpg'))
     window = CLOComparisonApp()
     window.show()
     sys.exit(app.exec_())
